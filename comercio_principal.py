f07006911a512b4672651919910f17a3652dfc70
Macacruzf <maca.cruz@duocuc.cl>
2024-06-08 18:12:20 -0400

    Update comercio_principal.py
    comienzo del menu

--- comercio_principal.py
+++ comercio_principal.py
@@ -1,1 +1,5 @@
-
+from libreria import agregar_producto, mostrar_inventario, procesar_venta, inventario
+while True:
+  print("Menu Comercio ALMA ZEN")
+  print("1. Agregar producto")
+  print("2. Mostrar inventario")
