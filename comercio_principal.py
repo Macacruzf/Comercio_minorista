ac4194f80a38134d345ed865f81901cd2d0e1b1a
xiomararomero98 <xio.romero@duocuc.cl>
2024-06-10 17:20:47 -0400

    Update comercio_principal.py
    algunos cambios realizados. Revisar

--- comercio_principal.py
+++ comercio_principal.py
@@ -1,23 +1,33 @@
-from libreria import agregar_producto, mostrar_inventario, procesar_venta, inventario
-while True:
-  print("Menu Comercio ALMA ZEN")
-  print("1. Agregar producto")
-  print("2. Mostrar inventario")
-  print("3. Procesar Venta")
-  print("4. Generar informe")
-  print("5. Salir")
- opcion=input("Ingrese una opción: (1-5)") 
-  if opcion == "1":
-    agregar_producto()
-  elif opcion == "2":
-    mostrar_inventario()
-  elif opcion == "3":
-     procesar_venta()
-  elif  opcion == "4":
-    generar_informe()
-  elif opcion == "5":
-        print("Saliendo del programa...")
-        break
-  else:
-        print("Opción inválida.")
+from Comercio_libreria import iniciar_sesion, agregar_producto, mostrar_inventario, procesar_venta, generar_informe
+
+def mostrar_menu():
+    print("\nMenu Comercio ALMA ZEN")
+    print("1. Agregar producto")
+    print("2. Mostrar inventario")
+    print("3. Procesar venta")
+    print("4. Generar informe")
+    print("5. Salir")
+
+def main():
+    if iniciar_sesion():
+        while True:
+            mostrar_menu()
+            opcion = input("Ingrese una opción (1-5): ")
+            if opcion == "1":
+                agregar_producto()
+            elif opcion == "2":
+                mostrar_inventario()
+            elif opcion == "3":
+                procesar_venta()
+            elif opcion == "4":
+                generar_informe()
+            elif opcion == "5":
+                print("Saliendo del programa...")
+                break
+            else:
+                print("Opción inválida.")
+
+if __name__ == "__main__":
+    main()
+
       
