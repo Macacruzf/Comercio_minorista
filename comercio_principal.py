e041e8678b415a5e03c796f80cf0513d38db10d1
xiomararomero98 <xio.romero@duocuc.cl>
2024-06-10 16:18:17 -0400

    Update comercio_principal.py

--- comercio_principal.py
+++ comercio_principal.py
@@ -3,3 +3,7 @@ while True:
   print("Menu Comercio ALMA ZEN")
   print("1. Agregar producto")
   print("2. Mostrar inventario")
+  print("3. Procesar Venta")
+  print("4. Generar informe")
+  print("5. Salir")
+ opcion=input("Ingrese una opción (1-5)") 
