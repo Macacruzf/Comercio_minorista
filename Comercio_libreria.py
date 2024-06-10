9069e21d2997fac53c449c2a5f45e4365be4fdd3
xiomararomero98 <xio.romero@duocuc.cl>
2024-06-10 16:11:50 -0400

    Update Comercio_libreria.py
    Cambios

--- Comercio_libreria.py
+++ Comercio_libreria.py
@@ -35,6 +35,45 @@ if iniciar_sesion():
                print(f"Codigo: {codigo}, Nombre: {nombre}, Cantidad: {cantidad}, Precio unitario: ${precio_unitario:.2f}, Total valor: ${total_valor:.2f}")
         else:
             print("El inventario esta vacio")
+
+    def procesar_venta():
+
+       nombre=input("Ingrese el nombre del producto a vender:")
+       cantidad= int(input("Ingrese la cantidad a vender:"))
+       for producto in inventario:
+          if producto[1] == nombre:
+            if producto[2]>= cantidad:
+                producto[2] -= cantidad
+                print("----Boleta----")
+                print(f"Venta procesada: Se vendieron {cantidad} unidades de '{nombre}'.")
+                precio_sin_iva= producto[2]*cantidad
+                precio_sin_iva= precio_sin_iva*1.19 #impuesto de iva
+                print(f"Venta procesada: Se vendio {cantidad} unidades de '{nombre}' por un total de ${precio_con_iva:.2f} (IVA 19% incluido).")
+             else:
+                print(f"No hay suficientes unidades de '{nombre}' en stock.")
+            break
+        else:
+           print(f"El producto '{nombre}' no está en el inventario.")
+
+def generar_informe():
+   if inventario:
+      total_productos= len(inventario)
+      total_cantidad=sum (producto[2] for producto in inventario)
+      total_valor= sum(producto[2]* producto[3] for producto in inventario)
+      print("Informe del inventario:")
+      print (f"Total de productos: {total_productos}")
+      print (f"Total de unidades en stock: {total_cantidad}")
+      print (f"Valor total del inventario: ${total_valor:.2f}")
+    else:
+       print ("El inventario está vacío.")
+       
+
+
+
+                
+                                                                                                       
+                
+                
            
             
      
