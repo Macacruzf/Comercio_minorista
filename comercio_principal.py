27e0c95735283bc8af9a9ed5c1f1e574b7d6a7e9
Macacruzf <maca.cruz@duocuc.cl>
2024-06-12 22:15:17 -0400

    Update comercio_principal.py
    pequeña modificacion

--- comercio_principal.py
+++ comercio_principal.py
@@ -1,30 +1,21 @@
 from Comercio_libreria import iniciar_sesion, agregar_producto, mostrar_inventario, procesar_venta
 
-def mostrar_menu():
+while True:
     print("\nMenu Comercio ALMA ZEN")
     print("1. Agregar producto")
     print("2. Mostrar inventario")
     print("3. Procesar venta")
     print("4. Salir")
-
-def main():
-    if iniciar_sesion():
-        while True:
-            mostrar_menu()
-            opcion = input("Ingrese una opción (1-5): ")
-            if opcion == "1":
-                agregar_producto()
-            elif opcion == "2":
-                mostrar_inventario()
-            elif opcion == "3":
-                procesar_venta()
-
-            elif opcion == "4":
-                print("Saliendo del programa...")
-                break
-            else:
-                print("Opción inválida.")
-
-if __name__ == "__main__":
-    main()
-
+    opcion = input("Seleccion una opción (1 - 4):   ")
+   
+    if opcion == "1":
+        agregar_producto()
+    elif opcion == "2":
+        mostrar_inventario()
+    elif opcion == "3":
+        procesar_venta()
+    elif opcion == "4":
+        print("Saliendo del programa...")
+        break
+    else:
+        print("Opción inválida.")
